0392943fe08a59d30cf6319f7c960abec13b13b4
Xiaomony <117056212+Xiaomony@users.noreply.github.com>
2024-05-02 23:42:40 +0800

    修复一些小的问题 以及一些细节的完善

--- src/comReader.cpp
+++ src/comReader.cpp
@@ -8,14 +8,17 @@ void comReader::loop()
 {
     com_cls();
     std::string command;
-    int index=0;
+    int index;
     do
     {
+        index=-1;
         printf("\n>>>");
         std::cin>>command;
         try
         {
-            index=std::stoi(command);
+            index=std::stoi(command)-1;
+            if (index>=listMaxlen||index<0)
+                index=-1;
         }
         catch(const std::invalid_argument)
         {
@@ -26,13 +29,10 @@ void comReader::loop()
                     index=i;
             }
         }
-        catch(const std::out_of_range)
-        {
-            continue;
-        }
         if (!index)
             break;
-        ((*this).*(list[index].clBack))();
+        if (index!=-1)
+            ((*this).*(list[index].clBack))();
     } while (true);
     
 }
@@ -43,6 +43,7 @@ void comReader::com_cls()
 {
     system("cls");
     msgSuc("====================Noita Saver====================\n");
+    printf("\033[0;34m");
     msgLog("输入操作：(数字/编号/括号内简写)");
     for (int i=0;i<listMaxlen;i++)
     {
@@ -51,16 +52,31 @@ void comReader::com_cls()
         // if ((i+1)%3==0)
         //     printf("\n");
     }
+    printf("\033[0m");
 }
 
 void comReader::com_save()
 {
     std::string name,comment;
-    printf("  存档名(可不填):");
     getchar();
-    getline(std::cin,name);
-    printf("  存档备注(可不填):");
+    do{
+        printf("存档名(可不填,限32个字):");
+        getline(std::cin,name);
+        if (name.size()>32)
+            msgErr("超出了32个字");
+        else
+            break;
+    }while(true);
+    
+    do{
+    printf("存档备注(可不填,限1024个字):");
     getline(std::cin,comment);
+        if (comment.size()>1024)
+            msgErr("超出了1024个字");
+        else
+            break;
+    }while(true);
+
     fu->save(inform(name,comment));
     msgSuc("保存成功");
 }
@@ -86,14 +102,16 @@ void comReader::com_qload()
 void comReader::com_log()
 {
     auto infos=fu->GetInfos();
-    msgLog("--------------------------------------------------------");
+    msgWrn("------------------------------------------------------------------------------------");
+    printf("\033[0;33m");
     std::cout<<"存档总数："<<infos.size()<<std::endl;
     for (int i=0;i<infos.size();i++)
     {
         auto &it=infos[i];
         std::cout<<i+1<<'\t'<<it.time.toString()<<"\t\t"<<it.name<<"\t\t"<<it.comment<<std::endl;
     }
-    msgLog("--------------------------------------------------------");
+    printf("\033[0m");
+    msgWrn("------------------------------------------------------------------------------------");
 }
 
 void comReader::com_mArchive()
